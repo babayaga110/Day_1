533a374c9fcd1b2d0ee4889f0e7db9d61ef9104d
babayaga110 <karan.19jics046@jietjodhpur.ac.in>
2021-12-01 20:40:13 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const countDownDate = new Date("Dec 25, 2021 00:00:00").getTime();
+const audioPlay = document.getElementById("myAudio");
+let x = setInterval(function() {
+
+  let now = new Date().getTime();
+    
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = pad(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+  let seconds = pad(Math.floor((distance % (1000 * 60)) / 1000));
+    
+  document.getElementById("hero").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hero").innerHTML = "Merry Christmas";
+    audioPlay.play()
+    var duration = 15 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+        return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
+
+    var end = Date.now() + (15 * 1000);
+
+    // go Buckeyes!
+    var colors = ['#bb0000', '#ffffff'];
+
+    (function frame() {
+    confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+    });
+    confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+    });
+
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    }
+    }());
+  }
+
+}, 1000);
+
+function pad(number) {
+    return number < 10 ? `0${number.toString()}` : number
+ }
